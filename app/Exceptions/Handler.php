e5df889ab1809b6569d5ab62d5ce7b2af12bf634
jocarr3 <josemanuel.carrillo@me.com>
2017-04-17 02:32:41 -0600

    Exeption handler little fix

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -7,6 +7,12 @@ use Illuminate\Auth\AuthenticationException;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Validation\ValidationException;
+use App\Transformers\errorTransformer;
+use League\Fractal\Resource\Collection;
+use League\Fractal\Pagination\IlluminatePaginatorAdapter;
+use \Fractal;
+use Illuminate\Contracts\Validation\Validator;
+
 
 class Handler extends ExceptionHandler
 {
@@ -51,6 +57,11 @@ class Handler extends ExceptionHandler
             return $this->unauthorize($request, $exception);
         }
 
+        if ($exception instanceof Illuminate\ValidationException\ValidationException) {
+            
+            return $this->validationFailed($request, $exception);
+        }
+        dd($exception->errors());
         return parent::render($request, $exception);
     }
 
@@ -63,19 +74,13 @@ class Handler extends ExceptionHandler
      */
     protected function unauthenticated($request, AuthenticationException $exception)
     {
-        $errors = [
-            "message" => "Unauthenticated",
-            "errors"  => [
-                "name" => [
-                        ""
-                ]],
-            "meta" => ""
-        ];
+        $exception = collect(['message' => 'Unauthenticated', 'errors' => '']);
+        
+        $message = Fractal::create('Unauthenticated', new errorTransformer())->serializeWith(new \Spatie\Fractalistic\ArraySerializer())->toArray();
 
         if ($request->expectsJson()) {
-            //return response()->json(['error' => 'Unauthenticated.'], 401);
-            //dd($request->headers);
-            return response()->json($errors, 401);
+
+            return response()->json($message, 401);
         }
 
         return redirect()->guest(route('login'));
@@ -104,8 +109,13 @@ class Handler extends ExceptionHandler
 
     protected function validationFailed($request, ValidationException $exception)
     {
+        $validator = new Validator();
+        $exception = collect(['message' => 'Unauthenticated', 'errors' => $validator]);
+        
+        $message = Fractal::create($exception, new errorTransformer())->serializeWith(new \Spatie\Fractalistic\ArraySerializer())->toArray();
+        //dd($Validator);
         if ($request->expectsJson()) {  
-            return response()->json('validation failed', 422);
+            return response()->json($message, 422);
         }
     }
 
